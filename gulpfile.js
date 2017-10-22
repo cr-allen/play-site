57e2add651cd02e17b6c839bc32e05d0707b6b27
Augustine LF <c.r.allen310@gmail.com>
2017-10-22 16:12:08 -0700

    compiled scss into css and added navbar

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('styles', () => {
+    gulp.src('./sass/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'))
+});
+gulp.task('default', () => {
+    gulp.watch('./sass/**/*.scss', ['styles']);
+});
